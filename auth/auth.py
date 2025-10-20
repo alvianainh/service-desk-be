46feaa49c8c470bd79db11d6efa9186d35fa184a
Alviana Inh <alvyana.inh@gmail.com>
2025-10-20 16:43:41 +0700

    fix hash password

--- auth/auth.py
+++ auth/auth.py
@@ -26,11 +26,22 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 security = HTTPBearer()
      
+# def hash_password(password: str):
+#     return pwd_context.hash(password)
+
 def hash_password(password: str):
+    password = password.encode("utf-8")[:72]  # truncate
     return pwd_context.hash(password)
 
-def verify_password(plain_password, hashed_password) -> bool:
+
+# def verify_password(plain_password, hashed_password) -> bool:
+#     return pwd_context.verify(plain_password, hashed_password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    plain_password = plain_password.encode("utf-8")[:72]
     return pwd_context.verify(plain_password, hashed_password)
+
+
 
 # def create_access_token(data: dict, expires_delta: timedelta = None) -> str:
 #     to_encode = data.copy()
