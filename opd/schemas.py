3d53ede55dd7dd8194ad4cb2ba0810b6f1c1f0b9
Alviana Inh <alvyana.inh@gmail.com>
2025-10-31 08:32:46 +0700

    fix opd updates

--- opd/schemas.py
+++ opd/schemas.py
@@ -1,13 +1,24 @@
 from pydantic import BaseModel
 from typing import Optional
 from uuid import UUID
+from fastapi import Form
 
 class OPDBase(BaseModel):
     opd_name: str
     description: Optional[str] = None
+    file_path: Optional[str] = None
 
-class OPDCreate(OPDBase):
-    pass
+class OPDCreate(BaseModel):
+    opd_name: str
+    description: Optional[str] = None
+
+    @classmethod
+    def as_form(
+        cls,
+        opd_name: str = Form(...),
+        description: Optional[str] = Form(None),
+    ):
+        return cls(opd_name=opd_name, description=description)
 
 class OPDResponse(OPDBase):
     opd_id: UUID
